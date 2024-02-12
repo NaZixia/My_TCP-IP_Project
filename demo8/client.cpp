4cc1aa2c071006fdda0426704b5b698f69d4c712
NaZixia <1600341701@qq.com>
2024-02-12 16:46:11 +0800

    5.4章节(5)

---
+++ demo8/client.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#define BUFF_SIZE 1024
+void error_handling(const char *message);
+
+int main(int argc,char *argv[])
+{
+    int sock;
+    struct sockaddr_in serv_addr;
+    char message[BUFF_SIZE];
+    int str_len=0;
+    int idx=0,read_len=0;
+
+    int32_t nub; //用于接收传输字符的数量
+
+    if(argc!=3){
+        printf("Usage:%s<IP> <port>\n",argv[0]);
+        exit(1);
+    }
+
+    sock=socket(PF_INET,SOCK_STREAM,0);
+    if(sock==-1){
+        error_handling("error serv_sock");
+    }
+
+    memset(&serv_addr,0,sizeof(serv_addr));
+    serv_addr.sin_family=AF_INET;
+    serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
+    serv_addr.sin_port=htons(atoi(argv[2]));
+
+    if(connect(sock,(struct sockaddr*)&serv_addr,sizeof(serv_addr))==-1)
+        error_handling("connect() error!");
+
+
+    scanf("%d",nub);
+    message[0] = (num >> 0) & 0xFF; // 最低有效字节
+    message[1] = (num >> 8) & 0xFF;
+    message[2] = (num >> 16) & 0xFF;
+    message[3] = (num >> 24) & 0xFF; // 最高有效字节
+    
+    fgets(&message[4],BUFF_SIZE-4,stdin);
+    write(sock,message,BUFF_SIZE);
+    read(sock,message,BUFF_SIZE-1);
+
+    printf("Message from server :%s \n",message);
+    printf("funtion read call count :%d \n",nub);
+    close(sock);
+    return 0;
+}
+
+void error_handling(const char *message){
+    fputs(message,stderr);
+    fputc('\n',stderr);
+    exit(1);
+}
