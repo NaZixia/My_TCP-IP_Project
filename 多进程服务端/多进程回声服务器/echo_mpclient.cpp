82232b911e296b8d006509df7245d7b6f5570e38
NaZixia <1600341701@qq.com>
2024-02-25 20:10:23 +0800

    10章:多进程回声服务端,客户端

--- 多进程服务端/多进程回声服务器/echo_mpclient.cpp
+++ 多进程服务端/多进程回声服务器/echo_mpclient.cpp
@@ -4,24 +4,36 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <sys/wait.h>
 
 #define BUF_SIZE 1024
 void error_handling(const char *message);
 void suc_handling(const char *message);
+void client_rec(int sig);
+void write_routine(int sock,char *buf);
+void read_routine(int sock,char *buf);
 
 int main(int argc,char *argv[])
 {
     int sock;
     struct sockaddr_in serv_adr;
     char message[BUF_SIZE];
-    int str_len;
 
     int recv_len,recv_cnt; 
+
+    struct sigaction act;
+    pid_t pid;
 
     if(argc!=2){
         printf("Usage:%s<IP> <port>\n",argv[0]);
         exit(1);
     }
+
+    act.sa_handler=client_rec;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags=0;
+    sigaction(SIGCHLD,&act,0);
+
 
     sock=socket(PF_INET,SOCK_STREAM,0);
     if(sock==-1){
@@ -38,24 +50,11 @@ int main(int argc,char *argv[])
     else
         puts("connect....");
 
-    while(1){
-        fputs("print q to quit\n",stdout);
-        fgets(message,sizeof(message),stdin);
-
-        if(!strcmp(message,"q\n")||!strcmp(message,"Q\n"))
-            break;
-
-        
-        str_len=write(sock,message,strlen(message));
-        
-        recv_len=0;
-        while(recv_len<str_len){
-            recv_cnt=read(sock,&message[recv_len],BUF_SIZE-1);
-            recv_len+=recv_cnt;
-        }
-        message[str_len]=0;
-        printf("Message from server :%s \n",message);
-    }
+    pid=fork();
+    if(pid==0)
+        read_routine(sock,message);
+    else
+        write_routine(sock,message);
     close(sock);
     return 0;
 }
@@ -69,4 +68,34 @@ void error_handling(const char *message){
 void suc_handling(const char *message){
     fputs(message,stderr);
     fputc('\n',stderr);
+}
+
+void client_rec(int sig){
+    pid_t pid;
+    int status;
+    pid=waitpid(-1,&status,WNOHANG);
+    printf("remove client:%d server succes\n",pid);
+}
+
+
+
+void write_routine(int sock,char *buf){
+    while(1){
+        fgets(buf,sizeof(buf),stdin);
+        if(!strcmp(buf,"q\n")||!strcmp(buf,"Q\n")){
+            shutdown(sock,SHUT_WR);
+            return;
+        }
+        write(sock,buf,strlen(buf));
+    }
+}
+
+void read_routine(int sock,char *buf){
+    while(1){
+            int recv_len=read(sock,buf,sizeof(buf));
+            if(recv_len==0)
+                return;
+            buf[recv_len]=0;
+            printf("Message from server :%s \n",buf);
+        }
 }
