1377b2595deba1ac0291fcf9d8276f916ec56d3f
NaZixia <1600341701@qq.com>
2024-02-27 20:20:32 +0800

    11保存消息的通信服务器

---
+++ 11进程间通信/2保存消息的回声服务器/echo_mpclient.cpp
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+
+#define BUF_SIZE 1024
+void error_handling(const char *message);
+void suc_handling(const char *message);
+void client_rec(int sig);
+void write_routine(int sock,char *buf);
+void read_routine(int sock,char *buf);
+
+int main(int argc,char *argv[])
+{
+    int sock;
+    struct sockaddr_in serv_adr;
+    char message[BUF_SIZE];
+
+    struct sigaction act;
+    pid_t pid;
+
+    if(argc!=2){
+        printf("Usage:%s<IP> <port>\n",argv[0]);
+        exit(1);
+    }
+
+    act.sa_handler=client_rec;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags=0;
+    sigaction(SIGCHLD,&act,0);
+
+
+    sock=socket(PF_INET,SOCK_STREAM,0);
+    if(sock==-1){
+        error_handling("error serv_sock");
+    }
+
+    memset(&serv_adr,0,sizeof(serv_adr));
+    serv_adr.sin_family=AF_INET;
+    serv_adr.sin_addr.s_addr=inet_addr("127.0.0.1");
+    serv_adr.sin_port=htons(atoi(argv[1]));
+
+    if(connect(sock,(struct sockaddr*)&serv_adr,sizeof(serv_adr))==-1)
+        error_handling("connect() error!");
+    else
+        puts("connect....");
+
+    pid=fork();
+    if(pid==0)
+        read_routine(sock,message);
+    else
+        write_routine(sock,message);
+    close(sock);
+    return 0;
+}
+
+void error_handling(const char *message){
+    fputs(message,stderr);
+    fputc('\n',stderr);
+    exit(1);
+}
+
+void suc_handling(const char *message){
+    fputs(message,stderr);
+    fputc('\n',stderr);
+}
+
+void client_rec(int sig){
+    pid_t pid;
+    int status;
+    pid=waitpid(-1,&status,WNOHANG);
+    printf("remove client:%d server succes\n",pid);
+}
+
+
+
+void write_routine(int sock,char *buf){
+    while(1){
+        fgets(buf,BUF_SIZE,stdin);
+        if(!strcmp(buf,"q\n")||!strcmp(buf,"Q\n")){
+            shutdown(sock,SHUT_WR);
+            return;
+        }
+        write(sock,buf,strlen(buf));
+    }
+}
+
+void read_routine(int sock,char *buf){
+    while(1){
+            int recv_len=read(sock,buf,BUF_SIZE);
+            if(recv_len==0)
+                return;
+            buf[recv_len]=0;
+            printf("Message from server :%s \n",buf);
+        }
+}
---
+++ 11进程间通信/2保存消息的回声服务器/echo_storeserv.cpp
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+#define BUF_SIZE 100
+void error_handling(const char *message);
+void suc_handling(const char *message);
+void client_server(int sig);
+
+int main(int argc,char *argv[])
+{
+    int serv_sock;      //服务端套接字
+    int clnt_sock;      //客户端套接字
+    int fds[2];
+
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in clnt_addr;
+    socklen_t clnt_adr_sz;
+
+    char message[BUF_SIZE];
+    int str_len,state;
+
+    pid_t pid;
+    struct sigaction act;
+
+    if(argc!=2){
+        printf("Usage:%s<port>\n",argv[0]);
+        exit(1);
+    }
+
+    //完成服务 结构体初始化
+    act.sa_handler=client_server;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags=0;
+    state=sigaction(SIGCHLD,&act,0);
+
+    serv_sock=socket(PF_INET,SOCK_STREAM,0);
+    if(serv_sock==-1){
+        error_handling("error serv_sock creat");
+    }
+
+    memset(&serv_addr,0,sizeof(serv_addr));
+    serv_addr.sin_family=AF_INET;
+    serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+    serv_addr.sin_port=htons(atoi(argv[1]));
+
+    if(bind(serv_sock,(struct sockaddr*)&serv_addr,sizeof(serv_addr))==-1)
+	    error_handling("bind()error");
+
+    if(listen(serv_sock,5)==-1)
+	    error_handling("listen() error"); 
+
+    int n=0;
+    pipe(fds);
+    pid=fork();
+    if(pid==0){
+        FILE *file=fopen("echomsg.txt","wt");
+        char msgbuf[BUF_SIZE];
+        int i,len;
+
+        for(i=0;i<10;i++){
+            len=read(fds[0],msgbuf,BUF_SIZE);
+            fwrite((void*)msgbuf,1,len,file);
+        }
+        fclose(file);
+        return 0;
+    }
+
+    while(1){
+        clnt_adr_sz=sizeof(clnt_addr);
+        clnt_sock=accept(serv_sock,(struct sockaddr*)&clnt_addr,&clnt_adr_sz);
+        if(clnt_sock==-1)
+            continue;
+        else{
+            printf("new Connect client %d \n",n+1);
+            n++;
+        }
+        pid=fork();   
+        if(pid==-1){
+            close(clnt_sock);
+            continue;
+        }
+        if(pid==0){
+            close(serv_sock);
+            while((str_len=read(clnt_sock,message,BUF_SIZE))!=0){
+                write(clnt_sock,message,str_len);   //back call
+                write(fds[1],message,str_len);
+            }
+            close(clnt_sock);
+            printf("child %d client disconnect\n",n+1);
+            ++n;
+            return 0;
+        }
+        else
+            close(clnt_sock);
+    }
+    
+    close(serv_sock);
+    return 0;
+
+}
+
+void error_handling(const char *message){
+    fputs(message,stderr);
+    fputc('\n',stderr);
+    exit(1);
+}
+
+void suc_handling(const char *message){
+    fputs(message,stderr);
+    fputc('\n',stderr);
+}
+
+void client_server(int sig){
+    pid_t pid;
+    int status;
+    pid=waitpid(-1,&status,WNOHANG);
+    printf("remove client:%d server succes\n",pid);
+}
