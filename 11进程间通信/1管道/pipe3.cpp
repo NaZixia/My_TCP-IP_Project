f4cb1765fd88db836b6460e53c3138b1b463995d
NaZixia <1600341701@qq.com>
2024-02-27 20:14:53 +0800

    11进程间通信

--- 11进程间通信/1管道/pipe3.cpp
+++ 11进程间通信/1管道/pipe3.cpp
@@ -7,26 +7,25 @@ using namespace std;
 
 int main(){
 
-    int fds[2];
+    int fds1[2],fds2[2];
     char str[]="who are you";
     char str2[]="thank you for your message";
     pid_t pid;
     char buf[BUF_SIZE];
 
-    pipe(fds);
+    pipe(fds1);
+    pipe(fds2);
 
     pid=fork();
     if(pid==0){
-        write(fds[1],str,sizeof(str));
-        sleep(2);
-        read(fds[0],buf,BUF_SIZE);
+        write(fds2[1],str,sizeof(str));
+        read(fds1[0],buf,BUF_SIZE);
         cout<<"child:"<<buf<<endl;
     }
     else{
-        read(fds[0],buf,BUF_SIZE);
-        write(fds[1],str2,sizeof(str2));
+        read(fds2[0],buf,BUF_SIZE);
+        write(fds1[1],str2,sizeof(str2));
         cout<<"fat:"<<buf<<endl;
-        sleep(3);
     }
     
 
