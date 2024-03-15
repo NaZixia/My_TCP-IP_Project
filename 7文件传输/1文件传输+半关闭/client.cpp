e1627aef3ae024f8daf9a293bd999e3c44707d94
NaZixia <1600341701@qq.com>
2024-03-15 08:19:16 +0800

    7 .1文件传输+半关闭

---
+++ 7文件传输/1文件传输+半关闭/client.cpp
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#define BUFF_SIZE 1024
+void error_handling(const char *message);
+
+int main(int argc,char *argv[])
+{
+    int sock;
+    struct sockaddr_in serv_addr;
+    char message[BUFF_SIZE];
+    char file_name[30];
+    int str_len=0;
+    int idx=0,read_len=0;
+
+    FILE *fp;
+    int32_t num; //用于接收传输字符的数量
+
+    if(argc!=2){
+        printf("Usage:%s <port>\n",argv[0]);
+        exit(1);
+    }
+
+    sock=socket(PF_INET,SOCK_STREAM,0);
+    if(sock==-1){
+        error_handling("error serv_sock");
+    }
+
+    memset(&serv_addr,0,sizeof(serv_addr));
+    serv_addr.sin_family=AF_INET;
+    serv_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
+    serv_addr.sin_port=htons(atoi(argv[1]));
+
+    if(connect(sock,(struct sockaddr*)&serv_addr,sizeof(serv_addr))==-1)
+        error_handling("connect() error!");
+
+    memset(file_name,0,sizeof(file_name));
+    fputs("file name:",stdout);
+    scanf("%s",file_name);
+
+    write(sock,file_name,sizeof(file_name));
+
+    fp= fopen("client.txt", "w"); 
+    if (fp == NULL)  
+    {  
+        printf("File:\t%s Can Not Open To Write!\n", file_name);  
+        exit(1);  
+    }
+
+    memset(message,0,BUFF_SIZE);
+    if((str_len=read(sock,message,BUFF_SIZE))!=0)
+        fwrite(message, sizeof(char), str_len, fp);
+ 
+    puts("recvived file data");
+    write(sock,"Thank you",10);
+    fclose(fp);  
+    close(sock);
+    return 0;
+}
+
+void error_handling(const char *message){
+    fputs(message,stderr);
+    fputc('\n',stderr);
+    exit(1);
+}
